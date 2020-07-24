b3a6c8f9c9743d1a0ef88a13c9a458e5de88d7f3
treasureBear94 <qvo7896@gmail.com>
2020-07-25 00:00:34 +0900

    Initial commit

---
+++ src/main/java/com/campfour/campsns/CampsnsApplication.java
@@ -0,0 +1,13 @@
+package com.campfour.campsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampsnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campfour/campsns/CampsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campfour.campsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
