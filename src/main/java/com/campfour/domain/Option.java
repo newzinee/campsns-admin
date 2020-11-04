da0c568f4270dcc9a91fc61dd534a9f0528319c3
treasureBear94 <qvo7896@gmail.com>
2020-11-05 01:30:00 +0900

    Option에서 대분류(구분) 삭제

--- src/main/java/com/campfour/domain/Option.java
+++ src/main/java/com/campfour/domain/Option.java
@@ -2,25 +2,29 @@ package com.campfour.domain;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+/**
+ * 편의시설 같은거
+ * 전기, 화장실, 놀이터.. 이런거
+ */
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Option {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "option_id")
     private Long id;
-    private String name;    // 이름
-    private String type;    // 대분류(구분)
+    private String value;    // 이름
     private String code;    // 앱에 저장된 이미지 이름
 
     @Builder
-    public Option(String name, String type, String code) {
-        this.name = name;
-        this.type = type;
+    public Option(String value, String code) {
+        this.value = value;
         this.code = code;
     }
 }
