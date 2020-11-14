9d82a5738a239840ad3f0e8c6cd7a9d4ac66ff44
Yoojin <qvo7896@gmail.com>
2020-11-14 22:05:50 +0900

    :sparkles: 목록, 수정화면으로 이동하는 버튼 만들기 (#11)
    * :sparkles: 목록 버튼 만들기
    * :sparkles: 수정 버튼 만들기

--- src/main/java/com/campfour/controller/CampController.java
+++ src/main/java/com/campfour/controller/CampController.java
@@ -17,6 +17,12 @@ public class CampController {
     private final CampRepository campRepository;
     private final KakaoConfig kakaoConfig;
 
+    /**
+     * 캠핑장 목록
+     * @param model
+     * @param pageable
+     * @return
+     */
     @GetMapping("/camps")
     public String camp(Model model, Pageable pageable) {
 
@@ -25,6 +31,13 @@ public class CampController {
         return "camp/campList";
     }
 
+    /**
+     * 캠핑장 상세보기
+     * @param id
+     * @param model
+     * @param pageable
+     * @return
+     */
     @GetMapping("/camps/{id}")
     public String campDetail(@PathVariable Long id, Model model, Pageable pageable) {
         Camp camp = campRepository.findById(id).orElseThrow();
@@ -32,8 +45,24 @@ public class CampController {
         model.addAttribute("prev", campRepository.findPrevById(id));
         model.addAttribute("next", campRepository.findNextById(id));
         model.addAttribute("camp", camp);
+        model.addAttribute("page", pageable);
         model.addAttribute("kakaoConfig", kakaoConfig);
         return "camp/campDetail";
+    }
+
+    /**
+     * 캠핑장 수정 페이지로 이동
+     * @param id
+     * @param model
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/camps/{id}/form")
+    public String campUpdateForm(@PathVariable Long id, Model model, Pageable pageable) {
+        Camp camp = campRepository.findById(id).orElseThrow();
+        model.addAttribute("camp", camp);
+        model.addAttribute("page", pageable);
+        return "camp/campUpdateForm";
     }
 }
 
