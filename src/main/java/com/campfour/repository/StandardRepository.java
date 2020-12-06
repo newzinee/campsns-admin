692b31b8b34462c35179e99fafcc854511c357af
treasureBear94 <qvo7896@gmail.com>
2020-12-06 23:53:50 +0900

    :sparkles: [NEW] 사전조사 도메인 설계

---
+++ src/main/java/com/campfour/repository/StandardRepository.java
@@ -0,0 +1,13 @@
+package com.campfour.repository;
+
+import com.campfour.domain.Standard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author yj
+ * @version 0.1.0
+ * @since 2020/12/06
+ */
+public interface StandardRepository extends JpaRepository<Standard, Long> {
+
+}
